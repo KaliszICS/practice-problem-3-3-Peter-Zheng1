e9d9eaf9184d06e9eaa5b3d37922d2d486801ed4
Peter-Zheng1 <343277125@tdsb.ca>
2026-03-31 15:34:58 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,56 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static String evenOrOdd(int num) {
+
+	if (num == 0){
+		return "Zero";
+	}
+    else if (num % 2 == 0){
+        return "Even";
+        
+    }
+    else {
+        return "Odd";
+        
+    }
+    
+}
+
+public static String positiveOrNegative(int num){
+    if (num > 0){
+        return "Positive";
+    }
+    
+    else if (num == 0){
+        return "Zero";
+    }
+    
+    else{
+        return "Negative"; 
+    }
+}
+
+
+public static String pluralize(String word){
+    word = word.toLowerCase();
+
+    if (word.endsWith("ey")){
+           return "eys";
+    }
+    
+	    else if (word.endsWith("y")){
+		    return "ies";
+
+	}
+            else if (word.endsWith("ife")){
+		        return "ives";
+	}
+		
+	else{
+		return "s";
+	}
+
+}
 
 }
